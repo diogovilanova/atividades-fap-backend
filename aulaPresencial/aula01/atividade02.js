e9616a471a42273e0076fdadcfa3e564dffaa211
ARTHUR HENRIQUE ALMEIDA FERREIRA <84420336+diogovilanova@users.noreply.github.com>
2023-07-20 15:55:01 -0300

    commit segunda atividade da aula01

---
+++ aulaPresencial/aula01/atividade02.js
@@ -0,0 +1,26 @@
+    let num1 = prompt("Digite um número: ");
+    if (num1 >=0) {
+        alert(`O número ${num1} é positivo`);
+    } else {
+        alert(`O número é negativo.`);
+    }
+
+    let num2 = prompt("Digite um número: ");
+    if (num2 % 2 === 0) {
+        console.log(`O número ${num1} é par`);
+    } else {
+        alert(`O número é impar`);
+    }
+
+    
+
+
+
+//instalar modo sync
+/*
+const readLine = require('readline-sync);
+
+var nome = readline.question("Digite o seu nome: ");
+console.log(`O nome digitado foi: ${nome}`);
+
+*/
