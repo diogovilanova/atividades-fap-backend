bc419673ca97f0738ffd998defece7ba9615c240
Diogo Vila Nova <diogo.vila27@gmail.com>
2023-08-01 16:45:46 -0300

    update atividade

--- aulaPresencial/aula03/atividade01.js
+++ aulaPresencial/aula03/atividade01.js
@@ -22,3 +22,40 @@ if (numeros.includes(searchNumber)) {
 } else {
     console.log("O número 25 não foi encontrado!");
 }
+
+//6°
+const lista1 = [1, 2, 3, 4, 5];
+const lista2 = [1, 2, 3, 4, 5];
+
+const novaLista = lista1.concat(lista2);
+
+//7°
+const numeros1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const numerosPares = numeros1.filter(numeros => numeros % 2 === 0);
+
+//8°
+const numeros2 = [3, 1, 4 , 1, 5, 9, 2, 6, 5, 3, 5];
+
+numeros2.sort((a, b) => a - b);
+console.log(numeros2)
+
+//9°
+const frutas1 = ['maça', 'banana', 'laranja', 'uva', 'manga'];
+
+const searchFruit = frutas1.indexOf('laranja');
+
+console.log(`O indice da fruta laranja é: ${searchFruit}`);
+
+//10°
+const parametro = ['maça', 'banana', 'laranja', 'uva', 'manga'];
+console.log(arrayTest(parametro));
+
+function arrayTest(parametro) {
+        return Array.isArray(parametro)
+}
+
+
+
+
+
