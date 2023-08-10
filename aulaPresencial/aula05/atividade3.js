50b25ee50c28c1a3dd7f82c23b2bf3847c145d25
Diogo Vila Nova <diogo.vila27@gmail.com>
2023-08-10 16:43:50 -0300

    Update atividade3.js

--- aulaPresencial/aula05/atividade3.js
+++ aulaPresencial/aula05/atividade3.js
@@ -1,43 +1,42 @@
 const readLine = require('readline-sync');
 
-// const nome = readLine.question("Qual seu nome? ");
+const nome = readLine.question("Qual seu nome? ");
 
-// function criarMensagemDeSaudacao(nome) {
-//     const mensagem = `Hello, ${nome}`;
-//     return mensagem;
-// }
-// console.log(criarMensagemDeSaudacao(nome));
+function criarMensagemDeSaudacao(nome) {
+    const mensagem = `Hello, ${nome}`;
+    return mensagem;
+}
+console.log(criarMensagemDeSaudacao(nome));
 
-// //1° 
-// const num1 = parseFloat(readLine.question("Digite o primeiro número: "));
-// const num2 = parseFloat(readLine.question("Digite o segundo número: "));
+//1° 
+const num1 = parseFloat(readLine.question("Digite o primeiro número: "));
+const num2 = parseFloat(readLine.question("Digite o segundo número: "));
 
-// function somar(num1, num2) {
-//     const soma = num1 + num2;
-//     return soma;
-// }
-// console.log(somar(num1, num2));
+function somar(num1, num2) {
+    const soma = num1 + num2;
+    return soma;
+}
+console.log(somar(num1, num2));
 
-// //2°
-// const num1 = parseInt(readLine.question("Digite o número: "));
+//2°
+const num1 = parseInt(readLine.question("Digite o número: "));
 
-// function ehPar(num1) {
-//     if (num1 % 2 == 0) {
-//     return true;
-//     } else {
-//     return false;
-//     }
-// }
-// console.log(ehPar(num1));
+function ehPar(num1) {
+    if (num1 % 2 == 0) {
+    return true;
+    } else {
+    return false;
+    }
+}
+console.log(ehPar(num1));
 
-// //3
-// const num1 = parseFloat(readLine.question("Digite o primeiro número: "));
-// const num2 = parseFloat(readLine.question("Digite o segundo número: "));
-// const num3 = parseFloat(readLine.question("Digite o terceiro número: "));
+//3
+const num1 = parseFloat(readLine.question("Digite o primeiro número: "));
+const num2 = parseFloat(readLine.question("Digite o segundo número: "));
+const num3 = parseFloat(readLine.question("Digite o terceiro número: "));
 
-// function maiorNumero(num1, num2, num3) {
-//     return Math.max(num1, num2, num3);
-// }
-// console.log(maiorNumero(num1, num2, num3));
+function maiorNumero(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+}
+console.log(maiorNumero(num1, num2, num3));
 
-//4°
