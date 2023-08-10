f2a3fa19ab2b20fc033f449314cb2f6a546b5607
Diogo Vila Nova <diogo.vila27@gmail.com>
2023-08-10 16:43:27 -0300

    Create atividade3.js

---
+++ aulaPresencial/aula05/atividade3.js
@@ -0,0 +1,43 @@
+const readLine = require('readline-sync');
+
+// const nome = readLine.question("Qual seu nome? ");
+
+// function criarMensagemDeSaudacao(nome) {
+//     const mensagem = `Hello, ${nome}`;
+//     return mensagem;
+// }
+// console.log(criarMensagemDeSaudacao(nome));
+
+// //1° 
+// const num1 = parseFloat(readLine.question("Digite o primeiro número: "));
+// const num2 = parseFloat(readLine.question("Digite o segundo número: "));
+
+// function somar(num1, num2) {
+//     const soma = num1 + num2;
+//     return soma;
+// }
+// console.log(somar(num1, num2));
+
+// //2°
+// const num1 = parseInt(readLine.question("Digite o número: "));
+
+// function ehPar(num1) {
+//     if (num1 % 2 == 0) {
+//     return true;
+//     } else {
+//     return false;
+//     }
+// }
+// console.log(ehPar(num1));
+
+// //3
+// const num1 = parseFloat(readLine.question("Digite o primeiro número: "));
+// const num2 = parseFloat(readLine.question("Digite o segundo número: "));
+// const num3 = parseFloat(readLine.question("Digite o terceiro número: "));
+
+// function maiorNumero(num1, num2, num3) {
+//     return Math.max(num1, num2, num3);
+// }
+// console.log(maiorNumero(num1, num2, num3));
+
+//4°
