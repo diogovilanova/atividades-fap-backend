2365e6d2adcb261366fad2e30d0427f461aeddb5
Diogo Vila Nova <diogo.vila27@gmail.com>
2023-08-25 11:44:33 -0300

    Commit da atividade 1

---
+++ aulaOnline/javascript/modulo01/01-javascript.js
@@ -0,0 +1,20 @@
+//declarando e imprimindo com valor undefined
+let variavelBoolean;
+let variavelBigInt;
+let variavelString;
+let variavelSymbol;
+console.log("\n",variavelBoolean,"\n", variavelBigInt,"\n", variavelString,"\n", variavelSymbol);
+
+//atribuindo e imprimindo com valor nulo 
+variavelBoolean = null;
+variavelBigInt = null;
+variavelString = null;
+variavelSymbol = null;
+console.log("\n",variavelBoolean,"\n", variavelBigInt,"\n", variavelString,"\n", variavelSymbol);
+
+//atribuindo valores ao tipos de cada uma das variáveis
+variavelBoolean = false;
+variavelBigInt = 123456789012345678901234567890n;
+variavelString = "Oi, sou Diogo Vila Nova!";
+variavelSymbol = Symbol("up");
+console.log("\n",variavelBoolean,"\n", variavelBigInt,"\n", variavelString,"\n", variavelSymbol);
