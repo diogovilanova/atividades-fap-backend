a6a692b8e38b3f7ef9d2ffef86245a9de78d2b9e
Diogo <diogo.vila27@gmail.com>
2023-09-28 16:28:40 -0300

    commit da aula 01

---
+++ aulaPresencial/aula01/main.ts
@@ -0,0 +1,24 @@
+let numero: number = 27;
+let texto: null = null;
+let nome: string = "Diogo";
+let logico: boolean = false;
+let lista: Array<string> = ["maçã", "pera"];
+let lista2: Array<any> = [1, "banana", 2, "uva"];
+let objeto: Object = {nome:"Diogo", idade: 27};
+
+let numero2: number = 27.12345;
+let teste: string = "Ads";
+
+console.log(numero2.toFixed(2));
+console.log(numero2.toPrecision(1));
+console.log(numero2.toString());
+console.log(parseInt("27"));
+console.log(parseFloat("27.12"));
+console.log(isNaN(numero2));
+console.log(isFinite(numero2));
+console.log(numero2.toExponential());
+
+
+
+
+
